e93db51d552213be3aba4e87b0468d155e7ef533
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2020-04-07 16:04:01 +0100

    so far so good
    Parameters

---
+++ practice.js
@@ -0,0 +1,12 @@
+var myn = 50;
+for (i=50; i<=100; i++) {
+    if (i%2===0) { 
+        console.log(i);
+        
+    }
+}
+// finding the even numbers <100 and >50 
+/*The loop checks the numbers as they progress from 50,51,52... obeying the condition
+ in the if statement*/
+ //i=50 is the starting point
+ //i<=100 is the condition or end point, while i++ is the increment.
